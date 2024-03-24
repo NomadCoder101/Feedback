bcd00d7d5bfac4af2af655cdd34ab9d275f38e14
NomadCoder101 <cbinary07@gmail.com>
2024-03-24 13:06:47 -0400

    image upload class

--- FeedBack/profiles/views.py
+++ FeedBack/profiles/views.py
@@ -1,10 +1,11 @@
 
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import CreateView
 
 from django.http import HttpResponseRedirect
 
-from .forms import ProfileForm
+#from .forms import ProfileForm
 
 from .models import User_Profile
 # Create your views here.
@@ -16,23 +17,30 @@ from .models import User_Profile
 #         for chunck in file.chunks():
 #             file.write(chunck)
             
-            
+ 
+class CreateProfileView(CreateView):   
+    
+    template_name ="profiles/create_profile.html"
+    model = User_Profile     
+    fields= '__all__'
+    success_url = "/profiles"
+      
 
-class CreateProfileView(View):
-    def get(self, request):
+# class CreateProfileView(View):
+#     def get(self, request):
         
-        form = ProfileForm()
-        return render(request, "profiles/create_profile.html", {'form':form})
+#         form = ProfileForm()
+#         return render(request, "profiles/create_profile.html", {'form':form})
 
-    def post(self, request):
+#     def post(self, request):
         
-        submitted_form = ProfileForm(request.POST,request.FILES)
-        if submitted_form.is_valid():
+#         submitted_form = ProfileForm(request.POST,request.FILES)
+#         if submitted_form.is_valid():
             
-            profile = User_Profile(image= request.FILES['user_image'])
-            profile.save()
-            return HttpResponseRedirect('/profiles')
+#             profile = User_Profile(image= request.FILES['user_image'])
+#             profile.save()
+#             return HttpResponseRedirect('/profiles')
         
-        return render(request, "profiles/create_profile.html", {'form':submitted_form})
+#         return render(request, "profiles/create_profile.html", {'form':submitted_form})
         
         
